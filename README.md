79f3a52758841c8d154661351afae3fa68c42128
Steven Ramirez Salgado <ramirezsteven2016@gmail.com>
2022-10-13 14:50:52 -0500

    entrando

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center"> Fever HUB </h1>
+<p align="center">Una biblioteca de pelis con React.JS consumiendo la API de TMDB</p>
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import { MovieCard } from '../components/MovieCard'
+
+import './MoviesGrid.css'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+export const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+
+    
+    getTopRatedMovies(topRatedUrl)
+  }, [])
+
+  return (
+    <div className="container">
+      <h2 className="title">Trending:</h2>
+      <div className="movies_container">
+        {topMovies.length === 0 && <p>Loading...</p>}
+        {topMovies.length > 0 && topMovies.map((movie)=>(
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  )
+}
